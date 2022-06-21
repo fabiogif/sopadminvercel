d73aef2b214edf9d55604f34154b82c1b7190cc2
fabioinfracommerce <fabio.santos.ext@infracommerce.com.br>
2022-06-21 18:24:06 -0300

    retorno do id

--- app/Http/Resources/OccurrenceResource.php
+++ app/Http/Resources/OccurrenceResource.php
@@ -16,6 +16,7 @@ class OccurrenceResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'title' => $this->title,
             'description' => $this->description,
