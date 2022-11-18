9f0a1a75f71d322d4d0e25f5e16835d9663926a4
fabioinfracommerce <fabio.santos.ext@infracommerce.com.br>
2022-11-17 21:37:53 -0300

    Adicionado google maps

--- resources/views/admin/pages/occurrences/_partials/form.blade.php
+++ resources/views/admin/pages/occurrences/_partials/form.blade.php
@@ -12,13 +12,13 @@ if (!isset($occurrences)) {
 }
 ?>
 <div class="row">
-    <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
         <div class="row">
             <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
                 <div class="form-group">
                     <label for="title">Nome Completo:</label>
-                    <input type="text" name="name" id="name" class="form-control" placeholder="Nome Completo"
-                        required value="{{ $occurrences->name ?? old('name') }}">
+                    <input type="text" name="name" id="name" class="form-control"
+                        placeholder="Informe o Nome Completo" required value="{{ $occurrences->name ?? old('name') }}">
                 </div>
             </div>
         </div>
@@ -26,7 +26,7 @@ if (!isset($occurrences)) {
             <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
                 <div class="form-group">
                     <label for="title">Titulo:</label>
-                    <input type="text" name="title" class="form-control" placeholder="Titulo" required
+                    <input type="text" name="title" class="form-control" placeholder="Informe o Titulo" required
                         value="{{ $occurrences->title ?? old('title') }}">
                 </div>
             </div>
@@ -35,16 +35,47 @@ if (!isset($occurrences)) {
             <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
                 <div class="form-group">
                     <label for="title">Endereço:</label>
-                    <input type="text" name="address" class="form-control" placeholder="Endereço" required
+                    <input type="text" name="address" id="location-input" class="form-control"
+                        placeholder="Informe o Endereço" autocomplete="off" required
                         value="{{ $occurrences->address ?? old('address') }}">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4  col-md-4  col-sm-4 col-xs-12">
+                <div class="form-group">
+                    <label for="title">Estado:</label>
+                    <input type="text" name="state" class="form-control " readonly
+                        id="administrative_area_level_1-input" required
+                        value="{{ $occurrences->state ?? old('state') }}">
+                </div>
+            </div>
+
+            <div class="col-lg-4  col-md-4  col-sm-4 col-xs-12">
+                <div class="form-group">
+                    <label for="title">Cidade:</label>
+                    <input type="text" name="city" id="locality-input" readonly class="form-control" required
+                        value="{{ $occurrences->city ?? old('city') }}">
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
                 <div class="form-group">
+                    <label for="title">CEP:</label>
+                    <input type="text" name="zip" id="postal_code-input" readonly class="form-control"
+                        placeholder="Informe o CEP" id="postal_code-input" required
+                        value="{{ $occurrences->zip ?? old('zip') }}">
+                </div>
+            </div>
+        </div>
+        <input type="hidden" value="BRASIL" id="country-input" />
+
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
+                <div class="form-group">
                     <label for="title">E-mail:</label>
-                    <input type="email" name="email" class="form-control" placeholder="E-mail" required
+                    <input type="email" name="email" class="form-control" placeholder="Informe o E-mail" required
                         value="{{ $occurrences->email ?? old('email') }}">
                 </div>
             </div>
@@ -53,8 +84,8 @@ if (!isset($occurrences)) {
             <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
                 <div class="form-group">
                     <label for="title">Telefone:</label>
-                    <input type="tel" name="phone" class="form-control phone" placeholder="Telefone" required
-                        value="{{ $occurrences->phone ?? old('phone') }}">
+                    <input type="tel" name="phone" class="form-control phone" placeholder="Informe o Telefone"
+                        required value="{{ $occurrences->phone ?? old('phone') }}">
                 </div>
             </div>
         </div>
@@ -63,14 +94,17 @@ if (!isset($occurrences)) {
             <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                 <div class="form-group">
                     <label for="latitude">Latitude:</label>
-                    <input type="text" name="latitude" class="form-control" placeholder="Latitude" disabled
+                    <input type="text" name="latitude" class="form-control" id="latitude"
+                        placeholder="Informe o
+                        Latitude" disabled
                         value="{{ $occurrences->latitude ?? old('latitude') }}">
                 </div>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                 <div class="form-group">
                     <label for="longitude">Longitude:</label>
-                    <input type="text" name="longitude" class="form-control" placeholder="Longitude" disabled
+                    <input type="text" name="longitude" class="form-control" id="longitude"
+                        placeholder="Informe o Longitude" disabled
                         value="{{ $occurrences->longitude ?? old('longitude') }}">
                 </div>
             </div>
@@ -138,6 +172,118 @@ if (!isset($occurrences)) {
             </div>
         </div>
     </div>
-
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <div class="card">
+            <div class="card-body">
+                <div id="map" style="height: 1000px"></div>
+            </div>
+        </div>
+    </div>
 </div>
+
 <!--row-->
+<script>
+    "use strict";
+    let map;
+
+    function initMap() {
+        const CONFIGURATION = {
+            "ctaTitle": "Checkout",
+            "mapOptions": {
+                "center": {
+                    "lat": -12.95307,
+                    "lng": -38.49706
+                },
+                "fullscreenControl": true,
+                "mapTypeControl": false,
+                "streetViewControl": true,
+                "zoom": 20,
+                "zoomControl": true,
+                "maxZoom": 22,
+                "mapId": ""
+            },
+            "mapsApiKey": "AIzaSyAKZUfI6dgn5kzjDSu9MKT84yhMW5UR5M0",
+            "capabilities": {
+                "addressAutocompleteControl": true,
+                "mapDisplayControl": true,
+                "ctaControl": true
+            }
+        };
+        const componentForm = [
+            'location',
+            'locality',
+            'administrative_area_level_1',
+            'country',
+            'postal_code',
+        ];
+        const getFormInputElement = (component) => document.getElementById(component + '-input');
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: {
+                lat: -12.95307,
+                lng: -38.49706
+            },
+            mapTypeControl: false,
+            fullscreenControl: CONFIGURATION.mapOptions.fullscreenControl,
+            zoom: CONFIGURATION.mapOptions.zoom,
+            streetViewControl: CONFIGURATION.mapOptions.streetViewControl
+        });
+        const marker = new google.maps.Marker({
+            map: map,
+            draggable: false
+        });
+        const autocompleteInput = getFormInputElement('location');
+        const autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {
+            fields: ["address_components", "geometry", "name"],
+            types: ["address"],
+        });
+        autocomplete.addListener('place_changed', function() {
+            marker.setVisible(false);
+            const place = autocomplete.getPlace();
+            if (!place.geometry) {
+                window.alert('No details available for input: \'' + place.name + '\'');
+                return;
+            }
+            renderAddress(place);
+            fillInAddress(place);
+            $("#longitude").val(place.geometry.location.lat());
+            $("#latitude").val(place.geometry.location.lng());
+        });
+
+        function fillInAddress(place) { // optional parameter
+            const addressNameFormat = {
+                'street_number': 'short_name',
+                'route': 'long_name',
+                'locality': 'long_name',
+                'administrative_area_level_1': 'short_name',
+                'country': 'long_name',
+                'postal_code': 'short_name',
+            };
+
+            const getAddressComp = function(type) {
+                for (const component of place.address_components) {
+                    if (component.types[0] === type) {
+                        return component[addressNameFormat[type]];
+                    }
+                }
+                return '';
+            };
+            getFormInputElement('location').value = getAddressComp('street_number') + ' ' + getAddressComp('route');
+            for (const component of componentForm) {
+                if (component !== 'location') {
+                    getFormInputElement(component).value = getAddressComp(component);
+                }
+            }
+        }
+
+        function renderAddress(place) {
+            map.setCenter(place.geometry.location);
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+        }
+    }
+
+    window.initMap = initMap;
+</script>
+<script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAKZUfI6dgn5kzjDSu9MKT84yhMW5UR5M0&libraries=places&callback=initMap&solution_channel=GMP_QB_addressselection_v1_cABC"
+    async defer></script>
--- resources/views/admin/pages/occurrences/create.blade.php
+++ resources/views/admin/pages/occurrences/create.blade.php
@@ -5,14 +5,17 @@
 @section('content_header')
     <h1 class="m-0 text-dark">Adicionar Ocorrência</h1>
 @stop
-
+<style>
+    #map {
+        height: 100%;
+    }
+</style>
 @section('content')
     <div class="row clearfix">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-6">
             <div class="card">
                 <div class="card-body">
-                    <form action="{{ route('occurrences.store') }}" class="form" method="POST"
-                        enctype="multipart/form-data">
+                    <form action="{{ route('occurrences.store') }}" class="form" method="POST" enctype="multipart/form-data">
                         @csrf
                         @include('admin.pages.occurrences._partials.form')
                     </form>
